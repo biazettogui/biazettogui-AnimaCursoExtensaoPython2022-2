d7b13b13cb75b28b579845f536a55fecc19e763e
Biazetto <116232259+biazettogui@users.noreply.github.com>
2022-10-21 16:29:27 +0000

    Código aula 19/10

---
+++ main.py
@@ -0,0 +1,39 @@
+# Meu primeiro projeto em Python.
+
+# print() = comando de saída
+print("Hello World")
+
+# Guardar String
+nome = "Guilherme Biazetto"
+
+# Guardar número inteiro
+idade = 20
+
+# Exibir nome (dentro da variável nome)
+print(nome)
+
+# Quando quiser exibir a frase "Minha idade é: " completando com o conteúdo da variável idade
+
+# str converter um número inteiro para string
+print("Minha idade é: "+str(idade))
+
+# \n não é tão necessario pois a linguagem pula a linha sozinho entre os prints
+print(f"Minha idade é: {idade}\n")
+
+# .format é um método de formatação de string
+print("Minha idade é {} anos". format(idade))
+
+# Quando quiser exibir a frase: "Meu nome é... e tenho... anos" trocando pelas variáveis nome e idade
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
+
+
+
+
+
+
+
+
+'''
+Comentario em bloco
+
+'''
